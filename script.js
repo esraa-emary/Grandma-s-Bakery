5c3961d7cb51a48831192c7866ea49da6f212de2
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-09-08 23:14:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const slides2 = document.querySelectorAll(".rev div");
+let slideIndex2 = 0;
+document.addEventListener("DOMContentLoaded", initializeSlider2);
+
+function initializeSlider2() {
+    if (slides2.length > 0) {
+        showSlides2(slideIndex2);
+        setInterval(nextslide2, 5000); 
+    }
+}
+
+function showSlides2(index) {
+    slideIndex2 = index % slides2.length;
+    slides2.forEach(slide => {
+        slide.classList.remove("displayslide2");
+    });
+
+    for (let i = 0; i < 1; i++) {
+        const slideToShow2 = (slideIndex2 + i) % slides2.length;
+        slides2[slideToShow2].classList.add("displayslide2");
+    }
+}
+
+function prevslide2() {
+    slideIndex2 --;
+    if (slideIndex2 < 0) {
+        slideIndex2 = slides2.length - 1;
+    }
+    showSlides2(slideIndex2);
+}
+
+function nextslide2() {
+    slideIndex2 ++;
+    if (slideIndex2 >= slides2.length) {
+        slideIndex2 = 0;
+    }
+    showSlides2(slideIndex2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function setActive(element) {
+    var sections = document.querySelectorAll('.nav-bar .page');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+    element.classList.add('active');
+}
+
+document.querySelector('.navbar__toggle').addEventListener('click', function() {
+    document.querySelector('.navbar').classList.toggle('active');
+});
+
